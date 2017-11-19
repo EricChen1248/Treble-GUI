cfa49e5d2b920a08f1fce44b336a44ada34e8db5
Eric Chen <EricChen1248@outlook.com>
2017-11-19 16:16:31 +0800

    Complete Base Form

--- Treble GUI/MainWindow.xaml.cs
+++ Treble GUI/MainWindow.xaml.cs
@@ -1,28 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Treble_GUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        public static string Account;
         public MainWindow()
         {
             InitializeComponent();
+            Frame.Content = new Login();
         }
+
+        private void CloseBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MaximizeBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        private void MinimizeBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        
+        private void TitleBar_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        
     }
 }
