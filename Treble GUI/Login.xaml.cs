70d0da67143789ebdf4d123118c5b18221de2913
Eric Chen <EricChen1248@outlook.com>
2017-11-19 16:16:37 +0800

    Created Basic Login

---
+++ Treble GUI/Login.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace Treble_GUI
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Check DataBase
+            MainWindow.Account = UserBox.Text;
+        }
+
+        private void PasswordBox_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                LoginBtn.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+            }
+        }
+    }
+}
