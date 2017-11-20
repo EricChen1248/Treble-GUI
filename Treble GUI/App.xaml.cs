dd108015f143915c47a17fcd29aabdaf305567b7
Eric Chen <EricChen1248@gmail.com>
2017-11-20 16:46:59 +0800

    Started object pool for pages

--- Treble GUI/App.xaml.cs
+++ Treble GUI/App.xaml.cs
@@ -1,11 +1,26 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Treble_GUI
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        private Dictionary<string, Page> pagePool = new Dictionary<string, Page>();
+
+        public Page GetNewPage(string pageName)
+        {
+            // TODO: Implement Page Spawning
+        }
+
+        public void DespawnPage(string pageName, Page page)
+        {
+            // TODO: Implement Page Despawning
+        }
+
+
     }
 }
