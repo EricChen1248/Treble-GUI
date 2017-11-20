8a6396d1a035af0df975321959e9664df5a6e8d6
Eric Chen <EricChen1248@outlook.com>
2017-11-20 09:23:51 +0800

    Creates Identicon for Tokens

---
+++ Treble GUI/Pages/TokenControl.xaml.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Drawing;
+using System.Windows;
+using System.Windows.Media;
+using Jdenticon;
+
+namespace Treble_GUI.Pages
+{
+    /// <summary>
+    /// Interaction logic for TokenControl.xaml
+    /// </summary>
+    public partial class TokenControl
+    {
+        private readonly string tokenID;
+        public TokenControl(string tokenID)
+        {
+            InitializeComponent();
+            MouseLeftButtonUp += TokenControl_MouseLeftButtonUp;
+            this.tokenID = tokenID;
+            DrawIcon();
+            TokenName.Content = tokenID;
+        }
+
+        private void TokenControl_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            // Handles Token Click
+            MessageBox.Show("Token is clicked");
+        }
+
+        private void DrawIcon()
+        {
+            var bitmap = new Bitmap(64, 64);
+            var g = Graphics.FromImage(bitmap);
+            Identicon.FromValue(tokenID, 64).Draw(g, new Rectangle(0, 0, 64, 64));
+
+            TokenIcon.Fill = new ImageBrush(Helper.BitmapToImageSource(bitmap));
+
+        }
+        
+
+    }
+}
