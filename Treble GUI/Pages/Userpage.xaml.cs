84c59079161329e435a66d8ed9d47fea3c9b9812
Eric Chen <EricChen1248@outlook.com>
2017-11-20 09:24:48 +0800

    Created Userpage

---
+++ Treble GUI/Pages/Userpage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls;
+
+namespace Treble_GUI.Pages
+{
+    /// <summary>
+    /// Interaction logic for Userpage.xaml
+    /// </summary>
+    public partial class Userpage
+    {
+        private readonly string Account;
+        public Userpage(string account)
+        {
+            InitializeComponent();
+            Account = account;
+            UserLabel.Content = Account;
+            TokenPanel.Children.Add(new TokenControl("National Taiwan University Library"));
+            TokenPanel.Children.Add(new TokenControl("National Taiwan University MB2"));
+
+            // Token Count
+            // Token Percentage
+
+        }
+    }
+}
