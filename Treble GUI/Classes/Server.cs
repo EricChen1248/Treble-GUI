5ca6f8c1af84d95a193cb3d006a94be5d6de0155
Eric Chen <EricChen1248@gmail.com>
2017-11-21 10:31:20 +0800

    Base Server System

---
+++ Treble GUI/Classes/Server.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+
+
+internal class Server
+{
+    private const string serverLocation = "Database/";
+    public string GetDatabase(string dataBaseName)
+    {
+        var results = ""
+        using( var sr = new StreamReader(serverLocation + dataBaseName))
+        {
+            results = sr.ReadToEnd();
+        }
+
+        return results;
+    }
+}
