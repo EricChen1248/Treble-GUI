cb36df0d1c82985b572c3779b54ff8e575bb5e29
Eric Chen <EricChen1248@gmail.com>
2017-11-25 17:04:28 +0800

    Updated page graphics

--- Treble GUI/Pages/Userpage.xaml.cs
+++ Treble GUI/Pages/Userpage.xaml.cs
@@ -9,21 +9,48 @@ namespace Treble_GUI.Pages
     /// </summary>
     public partial class Userpage
     {
-        private Dictionary<string, string> userData;
+        public List<TokenControl> tokens = new List<TokenControl>();
+        private static Dictionary<string, string> UserData;
+        public static string UserName => UserData["AccountName"];
+        public static int Money
+        {
+            get => int.Parse(UserData["Money"]);
+            set => UserData["Money"] = $"{value}";
+        }
+
         public Userpage(Dictionary<string, string> userData)
         {
             InitializeComponent();
 
-            this.userData = userData;
-            UserLabel.Content = userData["AccountName"];
-            MoneyLabel.Content = $"{int.Parse(userData["Money"]):C0}";
-            TokenPanel.Children.Add(new TokenControl("京華大樓 11F", "台北市大安區建國南路一段 291 巷 25 號", "2%"));
-            TokenPanel.Children.Add(new TokenControl("千里華夏 1F", "台北市萬華區大理街 170 巷 8 號", "10%"));
+            UserData = userData;
+
+            UserLabel.Content = UserName;
+            MoneyLabel.Content = $"{Money:C0}";
+            
+            tokens.Add(new TokenControl("京華大樓 11F", "台北市大安區建國南路一段 291 巷 25 號", "2%"));
+            tokens.Add(new TokenControl("千里華夏 1F", "台北市萬華區大理街 170 巷 8 號", "10%"));
+
+            foreach (var tokenControl in tokens)
+            {
+                TokenPanel.Children.Add(tokenControl);
+            }
 
             // Token Count
             // Token Percentage
 
         }
+
+        public void SaveUserData()
+        {
+            // TODO Save UserData
+        }
+
+        public void UpdateData()
+        {
+            UserLabel.Content = UserName;
+            MoneyLabel.Content = $"{Money:C0}";
+        }
+
 
 
     }
