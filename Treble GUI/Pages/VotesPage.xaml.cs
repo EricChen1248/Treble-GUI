6db45ef4b3bfef5248e33e6bc884389dda105204
Beckymark958 <beckymark958@gmail.com>
2017-11-25 22:55:11 +0800

    Started Voting System

---
+++ Treble GUI/Pages/VotesPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Treble_GUI.Pages
+{
+    /// <summary>
+    /// Interaction logic for VotesPage.xaml
+    /// </summary>
+    public partial class VotesPage : Page
+    {
+        public VotesPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CloseBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Instance.DestroyFloatingFrame();
+        }
+    }
+}
---
+++ Treble GUI/Pages/VotingSystem.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Treble_GUI.Pages
+{
+    /// <summary>
+    /// Interaction logic for VotingSystem.xaml
+    /// </summary>
+    public partial class VotingSystem : Page
+    {
+        public VotingSystem()
+        {
+            InitializeComponent();
+        }
+        private void CloseBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Instance.DestroyFloatingFrame();
+        }
+
+        private void VoteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Instance.NewFloatingFrame(new VotesPage());
+        }
+    }
+}
