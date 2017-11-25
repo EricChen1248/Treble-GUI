ae4b5a1bf68715030aeec96f02e0b4f46d0a9c95
Eric Chen <EricChen1248@gmail.com>
2017-11-25 17:03:50 +0800

    Added Back Label, Buy dialog, Identicon, Image

--- Treble GUI/Pages/TokenPage.xaml.cs
+++ Treble GUI/Pages/TokenPage.xaml.cs
@@ -1,29 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Jdenticon;
 
 namespace Treble_GUI.Pages
 {
     /// <summary>
     /// Interaction logic for TokenPage.xaml
     /// </summary>
-    public partial class TokenPage 
+    public partial class TokenPage
     {
-        public TokenPage()
+        private readonly string tokenID;
+        public TokenPage(string tokenID)
         {
             InitializeComponent();
+            this.tokenID = tokenID;
+            TokenLabel.Content = tokenID;
+            DrawIcon();
         }
-        
+
+
+        private void DrawIcon()
+        {
+            var bitmap = new Bitmap(64, 64);
+            var g = Graphics.FromImage(bitmap);
+            Identicon.FromValue(tokenID, 64).Draw(g, new Rectangle(0, 0, 64, 64));
+
+            TokenIcon.Fill = new ImageBrush(Helper.BitmapToImageSource(bitmap));
+        }
+
+
+        private void BackLabel_OnClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Instance.ChangeToToken(App.GetNewPage("userPage"));
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            App.DespawnPage("TokenPage", this);
+            MainWindow.Instance.NewFloatingFrame(new BuyToken(tokenID));
+        }
+
+
     }
 }
